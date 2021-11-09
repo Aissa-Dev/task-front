74a716e8124dd97ebbc1cffba284bf0701e135da
aissamihi <aissamihi@gmail.com>
2021-11-09 10:32:15 -0500

    remove duplicata in unit test

--- src/helpers/dataValidation.test.js
+++ src/helpers/dataValidation.test.js
@@ -1,5 +1,13 @@
 const { validate } = require("./dataValidation");
-test("Should output Errors for all inputs if they are empty", () => {
+
+const validValues = {
+  name: "elon",
+  company: "tesla",
+  email: "elon@tesla.com",
+  accept: true
+};
+
+test("Should output Errors for input if it is empty", () => {
   const formErrors = validate({
     name: "",
     company: "",
@@ -15,59 +23,28 @@ test("Should output Errors for all inputs if they are empty", () => {
 });
 
 test("Should output Errors for name if it is less then 2 chars", () => {
-  const formErrors = validate({
-    name: "c",
-    company: "tesla",
-    email: "aissamihi@gmail.com",
-    accept: true
-  });
-  expect(formErrors).toEqual({
-    name: "Name must be at least 2 chars"
-  });
+  const formErrors = validate({ ...validValues, name: "c" });
+  expect(formErrors).toEqual({ name: "Name must be at least 2 chars" });
 });
 
 test("Should output Errors for name with non alphabetic chars", () => {
-  const formErrors = validate({
-    name: "csd4",
-    company: "tesla",
-    email: "aissamihi@gmail.com",
-    accept: true
-  });
-  expect(formErrors).toEqual({
-    name: "Invalid name"
-  });
+  const formErrors = validate({ ...validValues, name: "csd4" });
+  expect(formErrors).toEqual({ name: "Invalid name" });
 });
 
 test("Should output Errors for email that is not respecting the email pattern", () => {
-  const formErrors = validate({
-    name: "elon",
-    company: "tesla",
-    email: "aissamihigmail.com",
-    accept: true
-  });
+  const formErrors = validate({ ...validValues, email: "aissamihigmail.com" });
   expect(formErrors).toEqual({
     email: "Email is required, please enter a valid email"
   });
 });
 
 test("Should output Errors for accept checkbox if it is not checked", () => {
-  const formErrors = validate({
-    name: "elon",
-    company: "tesla",
-    email: "aissamihi@gmail.com",
-    accept: false
-  });
-  expect(formErrors).toEqual({
-    accept: "Please accept to subscribe"
-  });
+  const formErrors = validate({ ...validValues, accept: false });
+  expect(formErrors).toEqual({ accept: "Please accept to subscribe" });
 });
 
 test("Should output empty errors object if it is all well validated", () => {
-  const formErrors = validate({
-    name: "elon",
-    company: "tesla",
-    email: "aissamihi@gmail.com",
-    accept: true
-  });
+  const formErrors = validate({ ...validValues });
   expect(formErrors).toEqual({});
 });
